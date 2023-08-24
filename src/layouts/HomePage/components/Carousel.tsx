207c42e424e67c3d4053822c0ce4a0caae811884
kdz <1062735832.com>
2023-08-23 23:58:02 -0700

    change backend port from 8080 to 5000

--- src/layouts/HomePage/components/Carousel.tsx
+++ src/layouts/HomePage/components/Carousel.tsx
@@ -12,7 +12,7 @@ export const Carousel = () => {
     useEffect(()=>{
         const fetchBooks = async ()=>{
             //const baseUrl:string = "http://library-springboot-env.eba-rdqrpvgc.us-east-2.elasticbeanstalk.com/api/books";
-            const baseUrl:string = "http://localhost:8080/api/books";
+            const baseUrl:string = "http://localhost:5000/api/books";
 
             const url:string = `${baseUrl}?page=0&size=9`;
 
