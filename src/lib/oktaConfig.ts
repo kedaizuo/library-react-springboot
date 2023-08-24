77ae4071cb4088f3ac3ceeed7bd6fb454669ff78
kdz <1062735832.com>
2023-08-23 17:07:35 -0700

    oktaConfig redirectUri changed to amplifyapp.com

--- src/lib/oktaConfig.ts
+++ src/lib/oktaConfig.ts
@@ -1,7 +1,7 @@
 export const oktaConfig={
     clientId:'0oaapjyq9kqA0timR5d7',
     issuer:'https://dev-27540031.okta.com/oauth2/default',
-    redirectUri:'http://localhost:3000/login/callback',
+    redirectUri:'https://main.d2zzk1frfnfe70.amplifyapp.com/login/callback',
     scopes:['openid', 'profile', 'email'],
     pkce:true,
     disableHttpsCheck:true,
