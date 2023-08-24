91687b326b0a446a5527cefebc946de452f87c92
kdz <1062735832.com>
2023-08-23 17:40:38 -0700

    change oktaConfig to amplify.com

--- src/lib/oktaConfig.ts
+++ src/lib/oktaConfig.ts
@@ -1,5 +1,5 @@
 export const oktaConfig={
-    clientId:'0oaapjyq9kqA0timR5d7',
+    clientId:'0oaay6hzpdDeir3gA5d7',
     issuer:'https://dev-27540031.okta.com/oauth2/default',
     redirectUri:'https://main.d2zzk1frfnfe70.amplifyapp.com/login/callback',
     scopes:['openid', 'profile', 'email'],
